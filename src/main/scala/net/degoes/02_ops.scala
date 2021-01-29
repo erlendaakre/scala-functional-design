92fed17e01fc151322690779b4386896a496991f
Erlend Aakre <erlend@aakre.io>
2021-01-29 11:08:08 +0000

    data transform - ex5 with fromOption()

--- src/main/scala/net/degoes/02_ops.scala
+++ src/main/scala/net/degoes/02_ops.scala
@@ -381,11 +381,12 @@ object contact_processing {
     def combine(leftColumn: String, rightColumn: String)(newName: String)(
       f: (String, String) => String
     ): SchemaMapping = SchemaMapping { list =>
-      val x = list.combine(leftColumn, rightColumn)(newName)(f)
-      x match {
-        case c: Some[ContactsCSV] => MappingResult.Success(Nil, c.get)
-        case None => MappingResult.Failure(s"Could not combine $leftColumn with $rightColumn" :: Nil)
-      }
+//      val x = list.combine(leftColumn, rightColumn)(newName)(f)
+//      x match {
+//        case c: Some[ContactsCSV] => MappingResult.Success(Nil, c.get)
+//        case None => MappingResult.Failure(s"Could not combine $leftColumn with $rightColumn" :: Nil)
+//      }
+      MappingResult.fromOption(list.combine(leftColumn, rightColumn)(newName)(f), s"Could not combine $leftColumn with $rightColumn")
     }
 
     /**
